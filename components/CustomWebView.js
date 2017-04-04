22976a8cae5626c2e472b575353c016f77eb1c38
Janne Saarela <janne.juhani@gmail.com>
2017-04-04 13:02:49 +0300

    Add ActivityIndicator

--- components/CustomWebView.js
+++ components/CustomWebView.js
@@ -3,15 +3,26 @@
  * @flow
  */
 
-import React from 'react';
-import {Platform, StyleSheet, View, WebView} from 'react-native';
+import React, {Component} from 'react';
+import {ActivityIndicator, Platform, StyleSheet, View, WebView} from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
+    centering: {
+        alignItems: 'center',
         justifyContent: 'center',
+        padding: 8,
+    },
+    container: {
         alignItems: 'center',
         backgroundColor: 'rgb(0, 122, 201)',
+        flex: 1,
+        justifyContent: 'center',
+    },
+    spinner: {
+        marginTop: 100,
+        height: 80,
+        position: 'absolute',
+        zIndex: 2,
     },
     text: {
         color: 'rgb(255, 255, 255)',
@@ -23,18 +34,38 @@ const styles = StyleSheet.create({
         width: '100%',
     },
 });
-const showSpinner = () => <Text>Loading...</Text>;
-function CustomWebView({uri}) {
-    return (
-        <View style={styles.container}>
-            <WebView
-                style={styles.webView}
-                source={{uri}}
-                scalesPageToFit
-                renderLoading={showSpinner}
-            />
-        </View>
-    );
+
+class CustomWebView extends Component { // eslint-disable-line react/prefer-stateless-function
+    constructor(props) {
+        super(props);
+        this.onLoadEnd = this.onLoadEnd.bind(this);
+        // Inner state is bad but at this point it easier
+        this.state = {
+            loading: true,
+        };
+    }
+    onLoadEnd() {
+        this.setState({loading: false});
+    }
+    render() {
+        const {uri} = this.props;
+        const {loading} = this.state;
+        return (
+            <View style={styles.container}>
+                <ActivityIndicator
+                    animating={loading}
+                    size="large"
+                    style={[styles.centering, styles.spinner]}
+                />
+                <WebView
+                    style={styles.webView}
+                    source={{uri}}
+                    scalesPageToFit
+                    onLoadEnd={this.onLoadEnd}
+                />
+            </View>
+        );
+    }
 }
 
 CustomWebView.propTypes = {
