915a810add1da8b58eb4e46d4222c7f4fdbebc05
Janne Saarela <janne.juhani@gmail.com>
2017-04-04 11:31:51 +0300

    Use CustomWebView, change classes to functions for now

--- components/Main.js
+++ components/Main.js
@@ -3,39 +3,11 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {StyleSheet, View, WebView} from 'react-native';
+import React from 'react';
+import CustomWebView from './CustomWebView';
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgb(0, 122, 201)',
-    },
-    text: {
-        color: 'rgb(255, 255, 255)',
-        height: 50,
-    },
-    webView: {
-        marginBottom: 50,
-        marginTop: 60,
-        width: '100%',
-    },
-});
-
-class Main extends Component { // eslint-disable-line react/prefer-stateless-function
-    render() {
-        return (
-            <View style={styles.container}>
-                <WebView
-                    style={styles.webView}
-                    source={{uri: 'https://www.reittiopas.fi/'}}
-                    scalesPageToFit
-                />
-            </View>
-        );
-    }
+function Main() {
+    return <CustomWebView uri="https://www.reittiopas.fi/" />;
 }
 
 export default Main;
--- components/Test.js
+++ components/Test.js
@@ -3,7 +3,7 @@
  * @flow
  */
 
-import React, {Component} from 'react';
+import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 // https://github.com/rebeccahughes/react-native-device-info
 import DeviceInfo from 'react-native-device-info';
@@ -23,16 +23,13 @@ const styles = StyleSheet.create({
     },
 });
 
-class Test extends Component { // eslint-disable-line react/prefer-stateless-function
-    render() {
-        console.log(`useragent: ${DeviceInfo.getUserAgent()}, model: ${DeviceInfo.getModel()}`);
-        return (
-            <View style={styles.container}>
-                <Text style={styles.text}>This is Test.js</Text>
-                <Text style={styles.text}>{`useragent: ${DeviceInfo.getUserAgent()}\n\nsystem name: ${DeviceInfo.getSystemName()}`}</Text>
-            </View>
-        );
-    }
+function Test() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>This is Test.js</Text>
+            <Text style={styles.text}>{`useragent: ${DeviceInfo.getUserAgent()}\n\nsystem name: ${DeviceInfo.getSystemName()}`}</Text>
+        </View>
+    );
 }
 
 export default Test;
