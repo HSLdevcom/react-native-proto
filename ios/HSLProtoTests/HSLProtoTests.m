6c11c422f73e35201b8086a8b846e3c13b1d24e5
Janne Saarela <janne.saarela@kehittamo.fi>
2017-04-18 14:38:38 +0300

    Rename app, add launchimages

--- ios/HSLProtoTests/HSLProtoTests.m
+++ ios/HSLProtoTests/HSLProtoTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface HSLReactNativeProtoTests : XCTestCase
+@interface HSLProtoTests : XCTestCase
 
 @end
 
-@implementation HSLReactNativeProtoTests
+@implementation HSLProtoTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
