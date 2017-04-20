049c1b3c0262f5e23ce2b8acc76ace14935640c7
Janne Saarela <janne.saarela@kehittamo.fi>
2017-04-20 13:44:59 +0300

    Add local html/js/css test file assets

---
+++ android/app/src/main/assets/web/test.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line
+setTimeout(function(){
+    document.querySelector('.test').innerText = 'Hello from android assets/web/test.js!';
+}, 1000);
---
+++ ios/web/test.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line
+setTimeout(function(){
+    document.querySelector('.test').innerText = 'Hello from ios web/test.js!';
+}, 1000);
