12e7bfcb4cb53db21a4436494a74b546c9d2eb4e
Janne Saarela <janne.saarela@kehittamo.fi>
2017-05-05 14:58:58 +0300

    Add image to SingleNews if there is one

--- app/components/SingleNews.js
+++ app/components/SingleNews.js
@@ -4,7 +4,7 @@
  */
 
 import React from 'react';
-import {Modal, Platform, StyleSheet, Text, TouchableOpacity, View, WebView} from 'react-native';
+import {Image, Modal, Platform, StyleSheet, Text, TouchableOpacity, View, WebView} from 'react-native';
 import Immutable from 'immutable';
 import colors from '../colors';
 import {removeMetaFromNewsHtml} from '../utils/helpers';
@@ -29,16 +29,21 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         flex: 1,
         justifyContent: 'center',
-        paddingLeft: 10,
-        paddingRight: 10,
-        paddingTop: (Platform.OS === 'ios') ? 63 : 53,
+        paddingTop: (Platform.OS === 'ios') ? 30 : 20,
+    },
+    image: {
+        height: 125,
+        marginTop: 5,
+        width: '100%',
     },
     title: {
         color: colors.brandColor,
         fontSize: 24,
+        width: '95%',
     },
     text: {
         color: 'black',
+        textAlign: 'center',
     },
     webView: {
         marginTop: 10,
@@ -49,6 +54,16 @@ const styles = StyleSheet.create({
 const closeModal = () => console.log('closed');
 
 function SingleNews({hide, singleNews}) {
+    let img = null;
+    if (singleNews.get('images').count() > 0) {
+        // TODO: can there be more than one image that we want to show?
+        // TODO: is this url valid every time?
+        const uri = `https://www.hsl.fi/sites/default/files/uploads/${singleNews.getIn(['images', 0, 'entity', 'filename'])}`;
+        img = (<Image
+            style={styles.image}
+            source={{uri}}
+        />);
+    }
     return (
         <Modal
             animationType="slide"
@@ -61,6 +76,7 @@ function SingleNews({hide, singleNews}) {
                     <Text style={styles.buttonText}>Sulje</Text>
                 </TouchableOpacity>
                 <Text style={[styles.text, styles.title]}>{singleNews.get('title')}</Text>
+                {img}
                 <WebView style={styles.webView} source={{html: removeMetaFromNewsHtml(singleNews.get('body').get('value'))}} />
             </View>
         </Modal>
