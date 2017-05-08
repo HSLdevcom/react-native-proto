063e4664a56003b1dcebc6d4f8ca7922fda01221
Janne Saarela <janne.saarela@kehittamo.fi>
2017-05-08 14:18:00 +0300

    Add composeWithDevTools

--- app/store.js
+++ app/store.js
@@ -1,11 +1,12 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {fromJS} from 'immutable';
+import {composeWithDevTools} from 'remote-redux-devtools';
 import {DEFAULT_STATE} from './intialState';
 import rootReducer from './reducers/index';
 
 const middleware = applyMiddleware(thunk);
 const initialState = fromJS(DEFAULT_STATE);
-const store = createStore(rootReducer, initialState, middleware);
+const store = createStore(rootReducer, initialState, composeWithDevTools(middleware));
 
 export default store;
