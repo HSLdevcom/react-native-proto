1cb80f1bdabe605b1d8eff8b1ad7f9b8bc1210b4
Janne Saarela <janne.saarela@kehittamo.fi>
2017-05-08 14:13:21 +0300

    Change citybike url

--- app/components/CityBikes.js
+++ app/components/CityBikes.js
@@ -7,7 +7,7 @@ import React from 'react';
 import CustomWebView from './CustomWebView';
 
 function CityBikes() {
-    return <CustomWebView uri="https://www.hsl.fi/kaupunkipyorat" />;
+    return <CustomWebView uri="https://www.hsl.fi/citybike" />;
 }
 
 export default CityBikes;
