0c3f7430a31c5ef081441d35e66d5ccdf721b371
Janne Saarela <janne.saarela@kehittamo.fi>
2017-05-12 14:55:13 +0300

    Add and link react-native-static-server and react-native-fs

--- android/app/src/main/java/com/hslfiproto/MainApplication.java
+++ android/app/src/main/java/com/hslfiproto/MainApplication.java
@@ -3,6 +3,8 @@ package com.hslfiproto;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
+import com.futurepress.staticserver.FPStaticServerPackage;
 import com.hslfiproto.BuildConfig;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
@@ -26,6 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFSPackage(),
+            new FPStaticServerPackage(),
             new VectorIconsPackage(),
             new RNDeviceInfo()
       );
