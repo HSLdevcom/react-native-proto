637fc17e83fee3f85819b692c9f0ef421a8f0521
Janne Saarela <janne.saarela@kehittamo.fi>
2017-05-18 19:59:20 +0300

    Add and link zip-lib

--- android/app/src/main/java/com/hslfiproto/MainApplication.java
+++ android/app/src/main/java/com/hslfiproto/MainApplication.java
@@ -3,6 +3,7 @@ package com.hslfiproto;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnziparchive.RNZipArchivePackage;
 import com.rnfs.RNFSPackage;
 import com.futurepress.staticserver.FPStaticServerPackage;
 import com.hslfiproto.BuildConfig;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNZipArchivePackage(),
             new RNFSPackage(),
             new FPStaticServerPackage(),
             new VectorIconsPackage(),
