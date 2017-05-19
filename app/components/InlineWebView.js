7e101f860f93eb43e2307a9f88d124043f3741f2
Janne Saarela <janne.saarela@kehittamo.fi>
2017-05-19 11:37:28 +0300

    Use navigator.geolocation.getCurrentPosition and save location to state, if location is set inject window.mock.geolocation.setCurrentPosition

--- app/components/InlineWebView.js
+++ app/components/InlineWebView.js
@@ -35,7 +35,28 @@ const styles = StyleSheet.create({
 });
 
 class InlineWebView extends Component { // eslint-disable-line react/prefer-stateless-function
-
+    // Inner state is bad but at this point it's easier
+    state = {
+        position: {},
+    };
+    componentDidMount() {
+        // TODO: do we want to get the position on every mount or keep it in state with some logic?
+        navigator.geolocation.getCurrentPosition((position) => {
+            if (position.coords) {
+                this.setState({
+                    position: {
+                        lat: position.coords.latitude,
+                        long: position.coords.longitude,
+                    },
+                });
+            }
+        }, error => console.log(error), {
+            enableHighAccuracy: true,
+            timeout: 20000,
+            maximumAge: 1000,
+        }
+        );
+    }
     onMessage = (event) => {
         console.log('msg: ', event.nativeEvent.data);
     }
@@ -45,16 +66,25 @@ class InlineWebView extends Component { // eslint-disable-line react/prefer-stat
     }
 
     render() {
-        const inlineJS = `
-            // var originalPostMessage = window.postMessage;
-            // var patchedPostMessage = function(message, targetOrigin, transfer) {
-            // originalPostMessage(message, targetOrigin, transfer);
-            // };
-            // patchedPostMessage.toString = function() {
-            // return String(Object.hasOwnProperty).replace('hasOwnProperty', 'postMessage');
-            // };
-            // window.postMessage = patchedPostMessage;
+        const {position} = this.state;
+        let inlineJS = `
+            var originalPostMessage = window.postMessage;
+            var patchedPostMessage = function(message, targetOrigin, transfer) {
+            originalPostMessage(message, targetOrigin, transfer);
+            };
+            patchedPostMessage.toString = function() {
+            return String(Object.hasOwnProperty).replace('hasOwnProperty', 'postMessage');
+            };
+            window.postMessage = patchedPostMessage;
         `;
+        if (position.lat && position.long) {
+            // If we have lat and long use mock
+            inlineJS = `
+                if (window.mock) {
+                    window.mock.geolocation.setCurrentPosition(${position.lat}, ${position.long});
+                }
+            `;
+        }
         return (
             <View
                 style={[styles.container]}
@@ -62,10 +92,10 @@ class InlineWebView extends Component { // eslint-disable-line react/prefer-stat
                 <WebView
                     ref={(c) => { this.webview = c; }}
                     style={styles.webView}
-                    source={{uri: (Platform.OS === 'ios') ? 'http://127.0.0.1:8080' : 'http://127.0.0.1:8080'}}
+                    source={{uri: (Platform.OS === 'ios') ? 'http://127.0.0.1:8080?mock' : 'http://127.0.0.1:8080?mock'}}
                     // file:///android_asset/web/index.html
                     scalesPageToFit
-                    // onMessage={this.onMessage} // this seems to break iOS
+                    // onMessage={this.onMessage} // this seems to break things
                     onNavigationStateChange={this.onNavigationStateChange}
                     injectedJavaScript={inlineJS}
                 />
