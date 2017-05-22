c0cfe3e23a96ecfc2531defe7efe17cce7da3455
Janne Saarela <janne.saarela@kehittamo.fi>
2017-05-22 16:22:31 +0300

    Add news actions tests

--- app/actions/__tests__/news.test.js
+++ app/actions/__tests__/news.test.js
@@ -2,15 +2,15 @@
 import {
     FETCHING_NEWS,
     FETCHING_NEWS_DONE,
-    // FETCHING_NEWS_ERROR,
-    // HIDE_SINGLE_NEWS,
-    // SHOW_SINGLE_NEWS,
+    FETCHING_NEWS_ERROR,
+    HIDE_SINGLE_NEWS,
+    SHOW_SINGLE_NEWS,
     fetchingNews,
     fetchNewsDone,
-    // fetchNewsError,
+    fetchNewsError,
     // fetchNewsData,
-    // hideSingleNews,
-    // showSingleNews,
+    hideSingleNews,
+    showSingleNews,
 } from '../news';
 
 describe('News actions', () => {
@@ -22,14 +22,42 @@ describe('News actions', () => {
             expect(fetchingNews()).toEqual(expected);
         });
     });
-    describe('REMOVE_COOKIE Action', () => {
-        it('has a type of REMOVE_COOKIE', () => {
+    describe('FETCHING_NEWS_DONE Action', () => {
+        it('has a type of FETCHING_NEWS_DONE', () => {
             const data = {};
             const expected = {
                 type: FETCHING_NEWS_DONE,
                 data,
             };
             expect(fetchNewsDone(data)).toEqual(expected);
+        });
+    });
+    describe('FETCHING_NEWS_ERROR Action', () => {
+        it('has a type of FETCHING_NEWS_ERROR', () => {
+            const error = {};
+            const expected = {
+                type: FETCHING_NEWS_ERROR,
+                error,
+            };
+            expect(fetchNewsError(error)).toEqual(expected);
+        });
+    });
+    describe('HIDE_SINGLE_NEWS Action', () => {
+        it('has a type of HIDE_SINGLE_NEWS', () => {
+            const expected = {
+                type: HIDE_SINGLE_NEWS,
+            };
+            expect(hideSingleNews()).toEqual(expected);
+        });
+    });
+    describe('SHOW_SINGLE_NEWS Action', () => {
+        it('has a type of SHOW_SINGLE_NEWS', () => {
+            const nid = '1';
+            const expected = {
+                type: SHOW_SINGLE_NEWS,
+                nid,
+            };
+            expect(showSingleNews(nid)).toEqual(expected);
         });
     });
 });
