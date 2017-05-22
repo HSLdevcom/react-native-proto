486ee22aae999bc343dcd7bc3028f6272cafe427
Janne Saarela <janne.saarela@kehittamo.fi>
2017-05-22 16:09:47 +0300

    Fix tests, update snapshots

--- __tests__/components/NewsFeed.test.js
+++ __tests__/components/NewsFeed.test.js
@@ -1,11 +1,27 @@
 // https://facebook.github.io/jest/blog/2016/07/27/jest-14.html
 import 'react-native';
 import React from 'react';
+import configureStore from 'redux-mock-store';
+import {fromJS} from 'immutable';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 import NewsFeed from '../../app/components/NewsFeed';
 
+const mockStore = configureStore();
+const news = fromJS({
+    data: [],
+});
+
 it('renders correctly', () => {
-    const tree = renderer.create(<NewsFeed />).toJSON();
+    const tree = renderer.create(
+        <NewsFeed
+            store={mockStore({
+                news,
+                fetchNewsData: jest.fn(),
+                hideSingleNews: jest.fn(),
+                showSingleNews: jest.fn(),
+            })}
+        />
+    ).toJSON();
     expect(tree).toMatchSnapshot();
 });
