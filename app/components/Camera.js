9bca547c18f3c8228759ec0e4c144c4619c96f73
Janne Saarela <janne.saarela@kehittamo.fi>
2017-05-31 14:30:36 +0300

    Add CameraRoll implementation, add view image after capture implementation

--- app/components/Camera.js
+++ app/components/Camera.js
@@ -4,12 +4,15 @@
  */
 
 import React, {Component} from 'react';
-import {Linking, StyleSheet, Text, View} from 'react-native';
+import {CameraRoll, Dimensions, Image, Linking, ScrollView, TouchableHighlight, StyleSheet, Text, View} from 'react-native';
 import Camera from 'react-native-camera';
 
+const {height, width} = Dimensions.get('window');
 const styles = StyleSheet.create({
     scrollView: {
         alignItems: 'center',
+        flexWrap: 'wrap',
+        flexDirection: 'row',
         justifyContent: 'center',
     },
     container: {
@@ -21,6 +24,10 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-end',
         alignItems: 'center',
     },
+    cameraroll: {
+        position: 'absolute',
+        top: 0,
+    },
     capture: {
         flex: 0,
         backgroundColor: '#fff',
@@ -28,6 +35,10 @@ const styles = StyleSheet.create({
         color: '#000',
         padding: 30,
         margin: 40,
+    },
+    disabled: {
+        backgroundColor: '#d2d2d2',
+        color: '#989898',
     },
     image: {
         width: '100%',
@@ -39,8 +50,22 @@ const styles = StyleSheet.create({
 */
 class CameraComponent extends Component {
     state = {
-        mediaUri: false,
+        data: {},
+        disableCapture: false,
+        showRoll: false,
+        photos: false,
     };
+    componentDidMount() {
+        this.getImages();
+    }
+    getImages = () => {
+        CameraRoll.getPhotos({
+            first: 20,
+            assetType: 'All',
+        })
+        .then(r => this.setState({photos: r.edges}))
+        .catch(err => console.log(err));
+    }
     readBarCode = (data) => {
         console.log('data: ', data);
         if (
@@ -51,19 +76,79 @@ class CameraComponent extends Component {
         }
     }
     takePicture = () => {
+        this.setState({disableCapture: true});
         // options can include for example location data
         const options = {};
         this.camera.capture({metadata: options})
         .then((data) => {
             console.log(data);
             if (data.mediaUri) {
+                this.getImages();
                 this.setState({
-                    mediaUri: data.mediaUri,
+                    data,
+                    disableCapture: false,
                 });
             }
         }).catch(err => console.error(err));
     }
+    toggleCameraRoll = () => {
+        this.setState({showRoll: !this.state.showRoll});
+    }
+    clearImage = () => {
+        this.setState({data: false});
+    }
     render() {
+        const {data, disableCapture, photos, showRoll} = this.state;
+        console.log(photos);
+        if (data.mediaUri) {
+            return (
+                <View style={styles.container}>
+                    <Image
+                        style={{
+                            width,
+                            height,
+                        }}
+                        source={{uri: data.mediaUri}}
+                    />
+                    <Text
+                        style={[styles.capture, styles.cameraroll]}
+                        onPress={this.clearImage}
+                    >
+                        [GO BACK]
+                    </Text>
+                </View>
+            );
+        } else if (showRoll) {
+            return (
+                <ScrollView contentContainerStyle={styles.scrollView}>
+                    {
+                    photos.map(p =>
+                        <TouchableHighlight
+                            key={p.node.image.uri}
+                            underlayColor="transparent"
+                        >
+                            <Image
+                                style={{
+                                    width: (width / 3),
+                                    height: (width / 3),
+                                }}
+                                source={{uri: p.node.image.uri}}
+                            />
+                        </TouchableHighlight>
+                    )
+                  }
+                    <Text
+                        style={[styles.capture]}
+                        onPress={this.toggleCameraRoll}
+                    >
+                        [GO BACK]
+                    </Text>
+                </ScrollView>
+            );
+        }
+        const captureButton = disableCapture ?
+            <Text style={[styles.capture, styles.disabled]}>[CAPTURE]</Text> :
+            <Text style={styles.capture} onPress={this.takePicture}>[CAPTURE]</Text>;
         return (
             <View style={styles.container}>
                 <Camera
@@ -73,7 +158,13 @@ class CameraComponent extends Component {
                     style={styles.preview} aspect={Camera.constants.Aspect.fill}
                     onBarCodeRead={this.readBarCode}
                 >
-                    <Text style={styles.capture} onPress={this.takePicture}>[CAPTURE]</Text>
+                    {captureButton}
+                    <Text
+                        style={[styles.cameraroll, styles.capture]}
+                        onPress={this.toggleCameraRoll}
+                    >
+                        [SHOW CAMERA ROLL]
+                    </Text>
                 </Camera>
             </View>
         );
